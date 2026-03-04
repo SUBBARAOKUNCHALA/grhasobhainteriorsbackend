9c3d895d8c895f06465fd819bfc0383bea48c2cb
subbarao.kunchala <subbaraokunchala1128@gmail.com>
2026-03-05 00:00:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const nodemailer = require("nodemailer");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+const User = require("./models/User");
+
+// Email Setup
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    console.log("BODY:", req.body); 
+    const { username, email, phone } = req.body;
+
+    const newUser = new User({ username, email, phone });
+    await newUser.save();
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: process.env.EMAIL_USER,
+      replyTo: email,   // THIS IS IMPORTANT
+      subject: "New Contact Lead Received",
+      html: `
+        <h3>New Customer Details</h3>
+        <p><b>Name:</b> ${username}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+        <br/>
+        <p>Click reply to contact this user directly.</p>
+      `
+    });
+
+    res.status(200).json({ message: "Data saved & mail sent" });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
