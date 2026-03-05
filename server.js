e0e71e1e7c398f6bd17641a266177779111d77e9
subbarao.kunchala <subbaraokunchala1128@gmail.com>
2026-03-05 19:23:03 +0530

    new chnages

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ const User = require("./models/User");
 // ✅ Fixed transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,  // App Password here
